e79e6540fbc669e3b036f567facb0914b824d537
hatim77100 <najoua-hatim@hotmail.fr>
2022-11-19 19:49:00 +0100

    update exo20

--- 7exosMoyens/exo1.js
+++ 7exosMoyens/exo1.js
@@ -1,31 +1,10 @@
 var readline = require("readline-sync");
-var tab = [3, 5, 10, 12, 1];
 // const ordre = tab.sort(dansLordre);
+var tab = [3, 5, 10, 12, 1];
 
 let ordre = (a, b) => {
   return a - b;
 };
-
-menu();
-var saisie = readline.questionInt("Quel est votre choix ? ");
-while (saisie !== 3) {
-  switch (saisie) {
-    case 1:
-      let chiffreSaisie = readline.questionInt(
-        "Quel chiffre voulez-vous ajouter ? "
-      );
-      let ajouter = ajouterUnChiffre(tab, chiffreSaisie);
-      console.log(ajouter);
-      menu();
-      break;
-    case 2:
-      console.log(ajouter.sort(ordre));
-      break;
-    case 3:
-      console.log("A+");
-      break;
-  }
-}
 
 const dansLordre = (param) => {
   ajouterUnChiffre();
@@ -35,9 +14,34 @@ const ajouterUnChiffre = (array, chiffre) => {
   return array.push(chiffre);
 };
 
+menu();
+var saisie = readline.questionInt("Quel est votre choix ? ");
+
+  switch (saisie) {
+    case 1:
+      let chiffreSaisie = readline.questionInt(
+        "Quel chiffre voulez-vous ajouter ? "
+      );
+
+      const ajouter = ajouterUnChiffre(tab, chiffreSaisie);
+      menu();
+      console.log(ajouter);
+      saisie = readline.questionInt("Quel est votre choix ?");
+      break;
+    case 2:
+      console.log(tab.sort(ordre));
+      break;
+    case 0:
+      console.log("A+");
+      break;
+    default:
+      console.log("Veuillez rechoisir !");
+  }
+
+
 function menu() {
   let txt = "1/ Ajouter un chiffre\n";
   txt += "2/ Afficher le tableau\n";
-  txt += "3/ quitter";
+  txt += "0/ quitter";
   return console.log(txt);
 }
